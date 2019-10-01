40adddc366c1df6352d0f1014c6d30788881b0ef
ryan tan <cloudzerox.tan@gmail.com>
2019-10-01 06:04:14 -0700

    Updates

---
+++ precache-manifest.8117333eed19b6f7885e218148e77e0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65fe42a6a1677e6b1002f347580a233d",
+    "url": "//tictac_extended/index.html"
+  },
+  {
+    "revision": "7be72877b72927e977d1",
+    "url": "//tictac_extended/static/css/main.a84da3f0.chunk.css"
+  },
+  {
+    "revision": "0fbabeb93e68157f6ae2",
+    "url": "//tictac_extended/static/js/2.a541aa0c.chunk.js"
+  },
+  {
+    "revision": "7be72877b72927e977d1",
+    "url": "//tictac_extended/static/js/main.83449c81.chunk.js"
+  },
+  {
+    "revision": "f6297c17179664de2404",
+    "url": "//tictac_extended/static/js/runtime~main.ab09f82b.js"
+  }
+]);
